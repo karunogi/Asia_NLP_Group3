b1c53e789ec2e668aac267a193707189f516aca1
Mike Hautsun <kyt9601@naver.com>
2020-10-02 07:44:46 +0900

    bot.py 불필요 변수 제거

--- chatapp/ChatFramework/bot.py
+++ chatapp/ChatFramework/bot.py
@@ -10,16 +10,15 @@ class Bot() :
 
     def __init__(self) :
         self.on_webtoon = 0
-        self.webtoon_step = 0
         self.context = ''
         self.gerne = []
         self.webtoon_data = np.load('chatapp/ChatFramework/data/webtoon/web_toon_data.npy',allow_pickle='TRUE').item()
 
         self.on_music = 0
-        self.music_step = 0
 
         self.on_movie = 0
-        self.movie_step = 0
+
+        self.step = 0
 
     # sentence (사용자의 질문)에 대한 챗봇의 답변을 받는 함수
     def get_answer(self, sentence) :
@@ -32,33 +31,33 @@ class Bot() :
             else : return '카테고리를 분류하지 못했습니다.<br/>다시 질문 해주시겠어요?'
 
         if self.on_webtoon :
-            if not self.webtoon_step :
+            if not self.step :
                 answer = '어떤 장르의 웹툰을 추천해 드릴까요?'
-                self.webtoon_step = 1
-            elif self.webtoon_step == 1 :
+                self.step = 1
+            elif self.step == 1 :
                 self.context, self.gerne = webtoon.webtoon_first_conv(sentence, self.context,self.gerne)
-                self.webtoon_step = 2
+                self.step = 2
                 answer = '음.. ' + self.gerne[0] + '..<br/>어떤 스토리의 웹툰을 추천해 드릴까요?'
-            elif self.webtoon_step == 2 :
+            elif self.step == 2 :
                 answer = webtoon.webtoon_second_conv(sentence, self.context, self.gerne)
                 answer = '이런 작품은 어떠세요?' + self.process_webtoon_answer(answer)
                 self.context = ''
                 self.gerne = []
-                self.on_webtoon = self.webtoon_step = 0
+                self.on_webtoon = self.step = 0
         elif self.on_music :
-            if not self.music_step :
+            if not self.step :
                 answer = '당신이 좋아하는 음악에 대해 알려주세요!<br/>예) 힙한 힙스터의 그루브'
-                self.music_step = 1
+                self.step = 1
             else :
                 answer = music.get_answer(sentence)
-                self.on_music = self.music_step = 0
+                self.on_music = self.step = 0
         elif self.on_movie :
-            if not self.movie_step :
+            if not self.step :
                 answer = '어떤 영화가 보고싶으세요?'
-                self.movie_step = 1
+                self.step = 1
             else :
                 answer = movie.get_answer(sentence)
-                self.on_movie = self.movie_step = 0
+                self.on_movie = self.step = 0
 
         return answer
 
